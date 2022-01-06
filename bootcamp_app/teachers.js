d24729e655bf629f3434300dfd0bab1d203bf202
Donny <donthephan@gmail.com>
2022-01-06 06:05:05 +0000

    update students.js and teachers.js with parameterized query to prevent sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,18 @@
 const { Pool } = require('pg');
 
+let cohortName = `%${process.argv[2]}%` || 'JUL02';
+let values = [ cohortName ];
+
+let queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
+`;
+
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -8,17 +21,7 @@ const pool = new Pool({
 });
 
 pool
-  .query(
-    `
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`
-  )
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((row) => {
       console.log(`${row.cohort}: ${row.teacher}`);
